69e14293c42592a8fa5ad07cfbadfce4dca018cc
Piers14 <61315972+Piers14@users.noreply.github.com>
2020-12-12 22:25:28 +0000

    Update data_preparation.R

--- data_preparation.R
+++ data_preparation.R
@@ -8,8 +8,20 @@ england_shot_tags <- get_shot_tags(england_shot_data)
 
 get_shot_tags <- function(df){
   suppressMessages(shot_tags <- tibble(shot_tags = df$tags) %>% hoist(shot_tags, tag = "id") %>%
-    unnest_wider(tag, names_sep = "") %>% rename_at(vars(contains(".")), funs(gsub("...", "_no_", ., fixed = TRUE))) %>%
+    unnest_wider(tag, names_sep = "") %>% rename_at(vars(contains("...")), funs(gsub("...", "_no_", ., fixed = TRUE))) %>%
     select(-shot_tags))
   return(shot_tags)
 }
 
+new_tags <- england_shot_tags %>% 
+  mutate(is_scored = ifelse(apply(., 1, function(x) any(x == 101, na.rm = TRUE)), 1, 0), 
+         is_blocked = ifelse(apply(., 1, function(x) any(x == 2101, na.rm = TRUE)), 1, 0), 
+         shot_type = ifelse(apply(., 1, function(x) any((x == 401 | x == 402), na.rm = TRUE)), "Foot", 
+                            ifelse(apply(., 1, function(x) any(x == 403, na.rm = TRUE)), "Head", NA)), 
+         is_counter  = ifelse(apply(., 1, function(x) any(x == 1901, na.rm = TRUE)), 1, 0))
+
+england_shot_pos <- tibble(shot_pos = england_shot_data$positions) %>%
+  hoist(shot_pos, y = "y", x = "x") %>%
+  unnest_wider(y, names_sep = "") %>%
+  unnest_wider(x, names_sep = "") %>%
+  dplyr::select(-positions)
